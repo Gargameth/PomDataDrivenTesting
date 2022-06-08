72dbdc0609003be081bdd83755598dee8e90b1e1
Gargameth <stragorhelm@gmail.com>
2022-06-08 10:56:24 +0200

    Add main file and first working login test.

---
+++ src/test/java/CollectionOfTests.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CollectionOfTests {
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+    WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    @Test
+    public void LoginTests() throws InterruptedException {
+        LoginPageTests loginPageTests = new LoginPageTests(driver);
+        loginPageTests.LoginWithValidCredentialsTest(wait);
+    }
+}
---
+++ src/test/java/LoginPageTests.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
+public class LoginPageTests {
+    WebDriver driver;
+    By usernameField = By.id("login-form-username");
+    By passwordField = By.id("login-form-password");
+    By loginBtn = By.name("login");
+
+    public LoginPageTests(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void LoginWithValidCredentialsTest(WebDriverWait wait) throws InterruptedException {
+        Dotenv dotenv = null;
+        dotenv = Dotenv.configure().filename("streetCreds.env").load();
+        driver.get("https://jira-auto.codecool.metastage.net/");
+        String username = dotenv.get("Real_Username");
+        String password = dotenv.get("Password");
+        wait.until(ExpectedConditions.presenceOfElementLocated(usernameField)).sendKeys(username);
+        wait.until(ExpectedConditions.presenceOfElementLocated(passwordField)).sendKeys(password);
+        wait.until(ExpectedConditions.presenceOfElementLocated(loginBtn)).click();
+        Thread.sleep(5000);
+    }
+}
